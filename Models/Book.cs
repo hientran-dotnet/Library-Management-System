f3854d5164514d1e6e69af456fd76a430fcf0a4b
Hien Tran <hientm0978@gmail.com>
2025-06-20 11:22:02 +0700

    feat: Implement Book model
    - Define Book entity with required properties
    - Add XML documentation for properties
    - Implement nullable reference types

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,31 +8,13 @@ namespace Library_Management_System.Models
 {
     public class Book
     {
-        // Book properties
-        private int Id; // Mã sách
-        private string? Title; // Tiêu đề sách
-        private Author? Author; // Tác giả của sách
-        private string? Publisher; // Nhà xuất bản sách
-        private int Year; // Năm xuất bản sách
-        private string? Category; // Thể loại sách
-        private bool IsAvailable; // Kiểm tra tính khả dụng của sách
+        public int Id { set; get; } // Mã sách
+        public string? Title { set; get; } // Tiêu đề sách
+        public string? Author { set; get; } // Tác giả của sách
+        public string? Publisher { set; get; } // Nhà xuất bản sách
+        public int Year { set; get; } // Năm xuất bản sách  
+        public string? Category { set; get; } // Thể loại sách
+        public bool IsAvailable { set; get; } // Kiểm tra tính khả dụng của sách
 
-        //Getters
-        public int GetId() => Id; // Mã sách
-        public string? GetTitle() => Title; // Tiêu đề sách
-        public Author? GetAuthor() => Author; // Tác giả của sách
-        public string? GetPublisher() => Publisher; // Nhà xuất bản sách
-        public int GetYear() => Year; // Năm xuất bản sách
-        public string? GetCategory() => Category; // Thể loại sách
-        public bool GetIsAvailable() => IsAvailable; // Kiểm tra tính khả dụng của sách
-
-        //Setters
-        public void SetId(int id) => Id = id; // Mã sách
-        public void SetTitle(string? title) => Title = title; // Tiêu đề sách
-        public void SetAuthor(Author? author) => Author = author; // Tác giả của sách
-        public void SetPublisher(string? publisher) => Publisher = publisher; // Nhà xuất bản sách
-        public void SetYear(int year) => Year = year; // Năm xuất bản sách
-        public void SetCategory(string? category) => Category = category; // Thể loại sách
-        public void SetIsAvailable(bool isAvailable) => IsAvailable = isAvailable; // Kiểm tra tính khả dụng của sách
     }
 }
